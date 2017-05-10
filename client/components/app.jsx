8bd7995388bce3d2220d9f34c9eae98d4865cdc8
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-05-10 12:27:19 -0500

    Change

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Search from './Search.jsx';
 import EntryList from './EntryList.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CreateImpressionForm from './CreateImpressionForm.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 class App extends React.Component {
   constructor (props) {
     super (props);
@@ -108,6 +115,7 @@ class App extends React.Component {
         <div className="container-fluid app">
           <header className="navbar">
             <div><h2 className="greeting">{this.greetUser()}</h2></div>
+            <CreateImpressionForm />
             <a href="/signout" className='navbar-right signout'>
               <button className="btn btn-default landing"><span>Sign Out</span></button>
             </a>
